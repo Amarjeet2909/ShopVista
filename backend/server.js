bab7e04d42bf9487fa0490e8067554866cf39de5
amarjeet <ak4348225@gmail.com>
2023-01-31 14:47:04 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+
+
+// Config
+dotenv.config({path:"backend/config/config.env"});
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+})
