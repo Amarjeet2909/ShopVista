0eb1fd9f7796da9ad820a012f0a348adfbe244f2
Amarjeet2909 <ak4348225@gmail.com>
2023-04-16 17:36:46 +0530

    Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import WebFont from "webfontloader";
 import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Route extact path="/" component={Home} />
 
       <Footer />
     </Router>
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,26 @@
+import React, { fragment } from "react";
+import { CgMouse } from "react-icons/all";
+import "./Home.css";
+
+/**
+React Fragment is a feature in React that allows you to return multiple elements from a React component by allowing 
+you to group a list of children without adding extra nodes to the DOM
+ */
+const Home = () => {
+    return (
+        <fragment>
+            <div className="banner">
+                <p>Welcome to ECOMMERCE</p>
+                <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+                <a href="container">
+                    <button>
+                        Scroll <CgMouse/>
+                    </button>
+                </a>
+            </div>
+        </fragment>
+    );
+};
+
+export default Home;
