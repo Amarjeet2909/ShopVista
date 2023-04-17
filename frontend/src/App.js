57de66d45d2b3b8fb10041d1880f5022430227b2
Amarjeet2909 <ak4348225@gmail.com>
2023-04-17 22:56:29 +0530

    Major Error Resolution of Frontend which Occured due to update in React-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
-import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router , Routes ,Route } from "react-router-dom";
 import WebFont from "webfontloader";
 import Footer from "./component/layout/Footer/Footer.js";
 import Home from "./component/Home/Home.js";
@@ -21,8 +21,9 @@ function App() {
   return (
     <Router>
       <Header />
-
-
+        <Routes>
+            <Route path='/' element={<Home/>} />
+        </Routes>
       <Footer />
     </Router>
   );
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,37 +1,30 @@
-import React, { fragment } from "react";
+import React, { Fragment , useEffect } from "react";
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
-import Product from "./Product.js";
-
-
-const product = {
-    name: "Blue TShirt",
-    images: [{url: "https://www.cit.ac.in"}],
-    price: "3000",
-    _id: "Amarjeet",
-};
 
 /**
 React Fragment is a feature in React that allows you to return multiple elements from a React component by allowing 
 you to group a list of children without adding extra nodes to the DOM
  */
 const Home = () => {
-    return (
-        <fragment>
+        return (
+        <Fragment>
             <div className="banner">
                 <p>Welcome to ECOMMERCE</p>
                 <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-                <a href="container">
+                <a href="#container">
                     <button>
                         Scroll <CgMouse/>
                     </button>
                 </a>
             </div>
-        </fragment>
 
+            <h2 className="homeHeading">Featured Products</h2>
+            
 
-    );
+        </Fragment>
+        );
 };
 
 export default Home;
