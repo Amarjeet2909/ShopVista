2c7b71d59101bb9f5d05b0b44b209e3a0bcc618b
Amarjeet2909 <ak4348225@gmail.com>
2023-04-25 22:51:27 +0530

    Making Product Component

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import "./ProductDetails.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../actions/productAction.js";
 
-const ProductDetails = () => {
-    return <div>Amar</div>;
+
+
+const ProductDetails = ({match}) => {
+
+    const dispatch = useDispatch();
+
+    const { product, loading, error } = useSelector(
+        (state) => state.productDetails
+    );
+
+    useEffect(() => {
+        dispatch(getProductDetails(this.props.match.params.id));
+    }, [dispatch, this.props.match.params.id]);
+    return (
+        <Fragment>
+            <div className="ProductDetails">
+                <div>
+                    <Carousel>
+                        {product.images &&
+                        product.images.map((item,i) => (
+                            <img
+                              className="CarouselImage"
+                              key = {item.url}
+                              src = {item.url}
+                              alt = {`${i} Slide`}
+                            />
+                        ))}      
+                    </Carousel>
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
 export default ProductDetails;
