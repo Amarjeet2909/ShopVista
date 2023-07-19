c9bf27cff0733efc29cc7709e0eb6b3ddf1a7f93
Amarjeet2909 <ak4348225@gmail.com>
2023-07-19 23:30:27 +0530

    Pagination updt

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,50 +1,122 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./Products.css";
 import { useSelector, useDispatch } from "react-redux";
-import { clearErrors, getProduct } from "../../actions/productAction.js";
-import Loader from "../layout/Loader/Loader.js";
-import ProductCard from "../Home/productCard.js";
-import { useParams } from 'react-router-dom';
+import { clearErrors, getProduct } from "../../actions/productAction";
+import Loader from "../layout/Loader/Loader";
+import ProductCard from "../Home/ProductCard";
 import Pagination from "react-js-pagination";
+import Slider from "@material-ui/core/Slider";
+import { useAlert } from "react-alert";
+import Typography from "@material-ui/core/Typography";
+import MetaData from "../layout/MetaData";
 
-// Function to show all products available
-const Products = ({match}) => {
-    const dispatch = useDispatch();
+const categories = [
+  "Laptop",
+  "Footwear",
+  "Bottom",
+  "Tops",
+  "Attire",
+  "Camera",
+  "SmartPhones",
+];
 
-    const [currentPage, setCurrentPage] = useState(1);
+const Products = ({ match }) => {
+  const dispatch = useDispatch();
 
-    const { keyword } = useParams();
+  const alert = useAlert();
 
-    const {products, loading, error, productsCount, resultPerPage} = useSelector(
-        (state) => state.products
-    );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
 
-    const setCurrentPageNo = (e)=> {
-        setCurrentPage(e);
-    };
+  const [ratings, setRatings] = useState(0);
 
-    useEffect(() => {
-        dispatch(getProduct(keyword, currentPage));
-    }, [dispatch,keyword,currentPage]);
+  const {
+    products,
+    loading,
+    error,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
 
-    return (
+  const keyword = match.params.keyword;
+
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
+
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
+  let count = filteredProductsCount;
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    dispatch(getProduct(keyword, currentPage, price, category, ratings));
+  }, [dispatch, keyword, currentPage, price, category, ratings, alert, error]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ? (
-                <Loader />
-                ) : (
-                <Fragment>
+          <MetaData title="PRODUCTS -- ECOMMERCE" />
+          <h2 className="productsHeading">Products</h2>
 
-                    <h2 className="productsHeading">Products</h2>
+          <div className="products">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
 
-                    <div className="products">
-                        {products && 
-                            products.map((product) => (
-                                <ProductCard key={product._id} product={product} />
-                            ))}
-                    </div>
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              min={0}
+              max={25000}
+            />
 
-                <div className="paginationBox">
-                <Pagination
+            <Typography>Categories</Typography>
+            <ul className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+
+            <fieldset>
+              <Typography component="legend">Ratings Above</Typography>
+              <Slider
+                value={ratings}
+                onChange={(e, newRating) => {
+                  setRatings(newRating);
+                }}
+                aria-labelledby="continuous-slider"
+                valueLabelDisplay="auto"
+                min={0}
+                max={5}
+              />
+            </fieldset>
+          </div>
+          {resultPerPage < count && (
+            <div className="paginationBox">
+              <Pagination
                 activePage={currentPage}
                 itemsCountPerPage={resultPerPage}
                 totalItemsCount={productsCount}
@@ -57,13 +129,13 @@ const Products = ({match}) => {
                 linkClass="page-link"
                 activeClass="pageItemActive"
                 activeLinkClass="pageLinkActive"
-                />
-                </div>
-                                
-                </Fragment>
-            )}
+              />
+            </div>
+          )}
         </Fragment>
-    );
+      )}
+    </Fragment>
+  );
 };
 
 export default Products;
