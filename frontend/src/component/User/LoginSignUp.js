56392c225d2fbb14df455ae3cde25e5c4252d6ae
Amarjeet2909 <ak4348225@gmail.com>
2023-10-07 21:11:35 +0530

    Register Functionality

--- frontend/src/component/User/LoginSignUp.js
+++ frontend/src/component/User/LoginSignUp.js
@@ -4,7 +4,7 @@ import Loader from "../layout/Loader/Loader";
 import { Link } from "react-router-dom";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
-
+import FaceIcon from "@material-ui/icons/Face";
 
 
 const LoginSignUp = () => {
@@ -16,8 +16,48 @@ const LoginSignUp = () => {
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
 
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const { name, email, password } = user;
+
+    const [avatar, setAvatar] = useState();
+    const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
+
     const loginSubmit = () => {
-        console.log("Form Submitted");
+        console.log("Login Form Submited");
+    };
+
+    const registerSubmit = (e) => {
+        e.preventDefault();
+        
+        const myForm = new FormData();
+
+        myForm.set("name", name);
+        myForm.set("email", email);
+        myForm.set("pasword", password);
+        myForm.set("avatar", avatar);
+        console.log("Sign Up Form Submited");
+    };
+
+    const registerDataChange = (e) => {
+        if (e.target.name === "avatar") {
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                if(reader.readyState === 2) {
+                    setAvatarPreview(reader.result);
+                    setAvatar(reader.result);
+                }
+            };
+
+            reader.readAsDataURL(e.target.files[0]);
+        } else {
+            setUser({ ...user, [e.target.name]: e.target.value });
+        }
     };
 
     const switchTabs = (e, tab) => {
@@ -71,6 +111,46 @@ const LoginSignUp = () => {
                         </div>
                         <Link to="/password/forgot">Forget Password ?</Link>
                         <input type="submit" value="Login" className="loginBtn" />
+                    </form>
+
+                    <form className="signUpForm" ref={registerTab} encType="multipart/form-data" onSubmit={registerSubmit}>
+                        <div className="signUpName">
+                            <FaceIcon />
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                required
+                                name="name"
+                                value={name}
+                                onChange={registerDataChange}
+                            />
+                        </div>
+                        <div className="signUpEmail">
+                            <FaceIcon />
+                            <input
+                                type="text"
+                                placeholder="Email"
+                                required
+                                email="email"
+                                value={email}
+                                onChange={registerDataChange}
+                            />
+                        </div>
+                        <div id="registerImage">
+                            <img src={avatarPreview} alt="Avatar Preview" />
+                            <input 
+                                type="file"
+                                name="avatar"
+                                accept="image/*"
+                                onChange={registerDataChange}
+                            />
+                        </div>
+                        <input
+                            type="submit"
+                            value="Register"
+                            className="signUpBtn"
+                        />
+
                     </form>
                 </div>
             </div>
