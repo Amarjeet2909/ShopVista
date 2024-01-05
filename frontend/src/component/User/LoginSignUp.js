8bc24d5dcd56353a4047aa069f16f5d85fef4f32
Amarjeet2909 <ak4348225@gmail.com>
2024-01-05 12:18:22 +0530

    Latest updated files

--- frontend/src/component/User/LoginSignUp.js
+++ frontend/src/component/User/LoginSignUp.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useRef, useState, useEffect } from "react";
 import "./LoginSignUp.css";
 import Loader from "../layout/Loader/Loader";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 import FaceIcon from "@material-ui/icons/Face";
@@ -9,11 +9,16 @@ import FaceIcon from "@material-ui/icons/Face";
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, login } from "../../actions/userAction";
 import { useAlert } from "react-alert";
+import { withRouter } from "react-router-dom";
 
+ 
+ 
 const LoginSignUp = () => {
     const dispatch = useDispatch();
     const alert = useAlert();
-    const history = useHistory();
+
+    const LoginSignUpWithRouter = withRouter(LoginSignUp);
+
 
     const { error, loading, isAuthenticated } = useSelector(
         (state) => state.user
@@ -78,7 +83,7 @@ const LoginSignUp = () => {
         }
 
         if(isAuthenticated) {
-            history.push("/account");
+            props.history.push("/account");
         }
     }, [dispatch, error, alert]);
 
